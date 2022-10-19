ff0a2f30f4697f3cccf44f5c9177fbdeb0466a64
Ed Reed <edreed@Eds-MacBook-Air.local>
2022-10-19 16:52:01 -0400

    refactor with static props

--- pages/lodge/[slug].js
+++ pages/lodge/[slug].js
@@ -7,37 +7,45 @@ import Info from '../../components/lodges/Info';
 import temples from '../../data/lodges';
 import styled from 'styled-components';
 
-const LodgePage = () => {
+const LodgePage = ({ currLodge }) => {
   const [lodge, setLodge] = useState(null);
-  const router = useRouter();
-  const slug = router.query.slug;
-  console.log(slug);
-
-  function getLodgeById(slug) {
-    const temple = temples.find(temple =>
-      temple.lodges.some(lodge => lodge.slug === slug)
-    );
-
-    return temple.lodges.filter(lodge => lodge.slug === slug)[0];
-  }
-  console.log(getLodgeById(slug));
-
   useEffect(() => {
-    setLodge(getLodgeById(slug));
-  }, [slug]);
-
+    setLodge(currLodge);
+  }, [currLodge]);
   return (
     <Layout title={lodge?.name}>
       <Container>
         <div className="info">
           <Images lodge={lodge} />
         </div>
-
         <Info lodge={lodge} />
       </Container>
     </Layout>
   );
 };
+
+export function getStaticPaths() {
+  const paths = temples.map(temple => ({
+    params: { slug: temple.lodges.map(lodge => lodge.slug)[0] },
+  }));
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export function getStaticProps({ params }) {
+  function getLodgeById(slug) {
+    const temple = temples.find(temple =>
+      temple.lodges.some(lodge => lodge.slug === params.slug)
+    );
+    return temple.lodges.filter(lodge => lodge.slug === slug)[0];
+  }
+
+  return {
+    props: { currLodge: getLodgeById(params.slug) },
+  };
+}
 
 const Container = styled.div`
   display: grid;
